7553eee129bb444d83349334db5255f79995f60d
Jeffrey Zhang <jeffreysfllo24@gmail.com>
2020-04-28 19:18:44 -0400

    Project setup commits - swift lint - git ignore - xcode project files

---
+++ example/StickyLayout/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StickyLayout
+//
+//  Created by Jeffrey Zhang on 2020-04-28.
+//  Copyright © 2020 Jeffrey Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
