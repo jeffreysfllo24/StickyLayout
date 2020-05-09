bd138d1842564694899f82bac142d85b6073b271
Jeffrey Zhang <jeffreysfllo24@gmail.com>
2020-05-09 17:29:41 -0400

    Removed unused override function in  StickyLayoutTests.

--- StickyLayout/StickyLayoutTests/StickyLayoutTests.swift
+++ StickyLayout/StickyLayoutTests/StickyLayoutTests.swift
@@ -13,14 +13,6 @@ class StickyLayoutTests: XCTestCase {
     
     let emptySet = Set<Int>()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testEmptyStickyConfigWithEmptyLayout() throws {
         let emptyStickyConfig = StickyLayoutConfig(stickyRowsFromTop: 0,
                                                                 stickyRowsFromBottom: 0,
